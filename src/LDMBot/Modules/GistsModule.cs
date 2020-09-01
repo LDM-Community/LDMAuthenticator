a984bb285035d20b9ce75d10b88076c1f75cf4da
papershredder432 <34615274+papershredder432@users.noreply.github.com>
2020-09-01 19:57:23 -0400

    Add files via upload

---
+++ src/LDMBot/Modules/GistsModule.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Newtonsoft.Json;
+
+namespace papershredder432.LDMAuthenticator.Modules
+{
+    [Name("Gists")]
+    [RequireContext(ContextType.Guild)]
+    public class GistsModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("Gist")]
+        [Summary("Sends a request.")]
+        private async Task Gist(string gistId)
+        {
+            if (gistId.StartsWith("https://gist.github.com/"))
+            {
+                gistId.Replace("https://gist.github.com/", "");
+            }
+            
+            var webRequest = WebRequest.Create($"https://api.github.com/gists/{gistId}") as HttpWebRequest;
+            if (webRequest == null) return;
+
+            webRequest.ContentType = "application/json";
+            webRequest.UserAgent = "Nothing";
+
+            using (var x = webRequest.GetResponse().GetResponseStream())
+            {
+                using (var sr = new StreamReader(x))
+                {
+                    var asJson = sr.ReadToEnd();
+                    var outPut = JsonConvert.DeserializeObject<Gist>(asJson);
+
+                    var embed = new EmbedBuilder();
+                    embed.WithTitle("New Auth Request");
+                    embed.WithDescription($"Requester: {outPut.Owner.Login}");
+                    embed.WithUrl(outPut.HtmlUrl.ToString());
+                    embed.WithThumbnailUrl(outPut.Owner.AvatarUrl.ToString());
+                    embed.AddField("Content", outPut.Files.LdmAuth.Content);
+
+                    await ReplyAsync(embed: embed.Build());
+                }
+            }
+        }
+
+        
+    }
+}
---
+++ src/LDMBot/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace papershredder432.LDMAuthenticator
+{
+    class Program
+    {
+        public static Task Main(string[] args)
+            => Startup.RunAsync(args);
+    }
+}
